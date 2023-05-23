cc8806fa5c3ba6703c2f088887e5f7417cc474f5
Tiago Lara <tilara20@gmail.com>
2023-05-23 11:00:17 -0300

    :sparkles: Add loja_dsapi, index.js

---
+++ api/index.js
@@ -0,0 +1,93 @@
+const restify = require("restify");
+const errors = require("restify-errors")
+
+const servidor = restify.createServer({
+    name: "loja_dsapi",
+    version: "1.0.0"
+})
+
+servidor.use(restify.plugins.acceptParser(servidor.acceptable));
+servidor.use(restify.plugins.queryParser());
+servidor.use(restify.plugins.bodyParser());
+
+servidor.listen(8001, function(){
+    console.log("%s executando em %s", servidor.name, servidor.url);
+});
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'loja_dsapi'
+    }
+});
+
+servidor.get('/', (req, res, next) => {
+    res.send('Bem-vindo(a) à API loja');   
+});
+
+servidor.get('/produtos', (req, res, next) => {
+    knex('produtos').then((dados) =>{
+        res.send(dados);
+    },next);
+    
+});
+
+servidor.get('/produtos/:id', (req, res, next) => {
+    const idProduto = req.params.id;
+    knex('produtos')
+    .where('id', idProduto)
+    .first()
+    .then((dados) =>{
+        if(!dados){
+            return res.send(new errors.BadRequestError('Este produto não foi encontrado'))
+        }
+        res.send(dados);
+    },next);
+});
+
+
+servidor.post('/clientes', (req, res, next) => {
+    const idProd = req.params.idProd;
+    knex('clientes')
+    .where('id', idProd)
+    .insert(req.body)
+    .then((dados) =>{
+        res.send(dados);
+    },next);
+    
+});
+
+
+
+
+
+servidor.put('/produtos/:idProd', (req, res, next) => {
+    const idProd = req.params.idProd;
+    knex('produto')
+    .where('id', idProduto)
+    .update(req.body)
+    .then((dados) =>{
+        if(!dados){
+            return res.send(new errors.BadRequestError('Este produto não foi encontrado'))
+        }
+        res.send('Produto atualizado');
+    },next);
+
+});
+
+servidor.del('/produtos/:idProd', (req, res, next) => {
+    const idProd = req.params.idProd;
+    knex('produto')
+    .where('id', idProduto)
+    .delete()
+    .then((dados) =>{
+        if(!dados){
+            return res.send(new errors.BadRequestError('Este produto não foi encontrado'))
+        }
+        res.send('Produto deletado');
+    },next);
+
+});
---
+++ loja_dsapi.sql
@@ -0,0 +1,56 @@
+CREATE database loja_dsapi;
+USE loja_dsapi;
+
+CREATE table cidades (
+	id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	nome VARCHAR(50)
+);
+
+CREATE table clientes (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nome VARCHAR(100),
+    altura DOUBLE,
+    nascimento DATE,
+    cidade_id INT NOT NULL,
+    FOREIGN KEY (cidade_id) REFERENCES cidades(id)
+);
+    
+CREATE table pedidos (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    horario DATETIME,
+    endereco VARCHAR(200),
+    cliente_id INT,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
+
+CREATE table categorias (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100)
+);
+
+CREATE table produtos (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nome VARCHAR(100),
+    preco DOUBLE,
+    quantidade DOUBLE,
+    categoria_id INT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+CREATE table pedidos_produtos (
+	pedido_id INT,
+    produto_id INT,
+    preco DOUBLE,
+    quantidade DOUBLE,
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
+
+INSERT INTO cidades(nome) 
+VALUES ('Porto Alegre'), ('Alvorada'), ('Cachoeirinha');
+INSERT INTO clientes(nome, altura, nascimento, cidade_id)
+VALUES ('Tiago', '1.65', '1990/12/04', '2'), ('Natália', '1.60', '2005/01/02', '1'), ('Marco', '1.55', '1986/04/20', '3')
+INSERT INTO categorias(nome)
+VALUES ('jogos'), ('livros'), ('esportes');
+INSERT INTO produtos(nome, preco, quantidade, categoria_id)
+VALUES ('Mk 11', '150', '1', '1'), ('Jogos Vorazes', '50', '2', '2'), ('Bola de Volei', '200', '1', '3');
